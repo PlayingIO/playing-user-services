0d3ea679074c30f1b7ca0e1d729a613f38af1306
forafish <fora.luo@gmail.com>
2017-09-14 18:53:10 +0800

    Refactoring user-model

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -7,48 +7,46 @@ import timestamps from 'mongoose-timestamp';
  * "roles" are assigned to a user to grant the permissions
  */
 const fields = {
-  username: { type: 'String', unique: true, required: true }, // login (maybe same as email or mobile)
   email: { type: 'String', unique: true, sparse: true },      // credential email
   mobile: { type: 'String', unique: true, sparse: true },     // credential mobile
+  username: { type: 'String', unique: true, required: true }, // login (maybe same as email or mobile)
   nickname: { type: 'String' },  // display name
   firstName: { type: 'String' }, // real name
   lastName: { type: 'String' },  // real name
   password: { type: 'String', required: true },
   groups: [{ type: 'ObjectId' }],
   roles: [{ type: 'ObjectId' }],
-  gender: { type: 'String', enum: ['MALE', 'FEMALE', 'UNKNOWN'], default: 'MALE' },
-  avatar: { type: 'Mixed' }, // TODO blob schema
-  intro: { type: 'String', default: '' },
-  birthday: { type: 'Date' },
-  company: { type: 'String' },
+
+  avatar: { type: 'Mixed' }, // blob schema
   city: { type: 'String' },
-  statistics: {
-    loginCount: { type: 'Number', default: 0 },
-    followersCount: { type: 'Number', default: 0 },
-    followeesCount: { type: 'Number', default: 0 },
-    browseNum: { type: 'Number', default: 0 },
-    bookCount: { type: 'Number', default: 0 }
-  },
-  socials: {
-    wechat: {
-      unionId: { type: 'String' },
-      nickname: { type: 'String' },
-      status: { type: 'Number' }
-    },
-  },
-  alerts: { type: 'Mixed' }, // last read time of each kind of alerts
-  // e.g. { <alertId>: new Date() }
-  invitation: { type: 'String' }, // invitation
+  company: { type: 'String' },
+  birthday: { type: 'Date' },
+  gender: { type: 'String', enum: ['MALE', 'FEMALE', 'UNKNOWN'], default: 'MALE' },
+  intro: { type: 'String', default: '' },
+  status: { type: 'Number', default: 0 },
   isAdministrator: { type: 'Boolean' }, // admin account
   isOfficial: { type: 'Boolean' }, // official account
-  status: { type: 'Number', default: 0 },
-  extras: {
-    lastDeviceUDID: { type: 'String' }, // last device
-    invitationDeviceUDID: { type: 'String' }, // device being invitated
-  }
+
+  alerts: { type: 'Mixed' },  // last read time of each kind of alerts
+                              // e.g. { <alertId>: new Date() }
+  devices: {
+    android: [{ type: 'Mixed' }], // android
+    ios: [{ type: 'Mixed' }],     // iphone
+    web: [{ type: 'Mixed' }],     // web browser
+    last: { type: 'Mixed' },      // last device
+    invited: { type: 'Mixed' },   // device being invitated
+  },
+  preferences: { type: 'Mixed' }, // preferences
+  invites: { type: 'Mixed' }, // invitation
+  socials: {
+    wechat: { type: 'Mixed' },
+    facebook: { type: 'Mixed' },
+    github: { type: 'Mixed' },
+  },
+  statistics: { type: 'Mixed' },
 };
 
-export default function(app, name) {
+export default function model(app, name) {
   const mongoose = app.get('mongoose');
   const schema = new mongoose.Schema(fields);
   schema.plugin(timestamps);
@@ -56,3 +54,5 @@ export default function(app, name) {
   schema.plugin(plugins.softDelete);
   return mongoose.model(name, schema);
 }
+
+model.schema = fields;
