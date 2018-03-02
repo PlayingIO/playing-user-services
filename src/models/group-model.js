026b724c2021c909a0741dd69d72d8262c7f7465
forafish <fora.luo@gmail.com>
2018-03-02 19:00:36 +0800

    refactoring the user/group/role/permission structures

--- src/models/group-model.js
+++ src/models/group-model.js
@@ -2,19 +2,14 @@ import mongoose from 'mongoose';
 import { plugins } from 'mostly-feathers-mongoose';
 import { models as contents } from 'playing-content-services';
 
+import { role } from './permission-schema';
+
 const options = {
   timestamps: true
 };
 
-const role = {
-  _id: false,
-  role: { type: String },                   // name of the role
-  permits: { type: 'Mixed' },               // map of all permissions
-};
-
 /*
  * A "group" is a collection of users.
- * "roles" are assigned to a group to grant the permissions.
  */
 const fields = {
   groupname: { type: String, required: true, unique: true },
@@ -22,7 +17,8 @@ const fields = {
   label: { type: String, required: true },   // display label
   description: { type: String },             // brief description of the group
   image: contents.blob.schema,               // image which represents the group
-  roles: [role]                              // defines the roles/permissions
+  roles: [role],                             // defines the roles/permissions
+  owner: { type: 'ObjectId' },               // owner who created the group
 };
 
 export default function model (app, name) {
--- src/models/user-model.js
+++ src/models/user-model.js
@@ -12,38 +12,42 @@ const options = {
 const fields = {
   email: { type: String, unique: true, sparse: true },      // credential email
   mobile: { type: String, unique: true, sparse: true },     // credential mobile
-  username: { type: String, unique: true, required: true }, // login (maybe same as email or mobile)
-  nickname: { type: String },     // display name
-  firstName: { type: String },    // real name
-  lastName: { type: String },     // real name
+  username: { type: String, unique: true, required: true }, // credential username
+  nickname: { type: String },              // display name
+  firstName: { type: String },             // real name
+  lastName: { type: String },              // real name
   password: { type: String, required: true },
-  groups: [{ type: 'ObjectId' }],
-  roles: [{ type: 'ObjectId' }],
+  avatar: { type: 'Mixed' },               // blob schema
 
-  avatar: { type: 'Mixed' },      // blob schema
+  groups: [{
+    group: { type: 'ObjectId' },           // group id
+    myroles: [{ type: 'String' }]          // my roles in this group
+  }],
+
   city: { type: String },
   company: { type: String },
   birthday: { type: Date },
   gender: { type: String, enum: ['MALE', 'FEMALE', 'UNKNOWN'], default: 'MALE' },
   intro: { type: String, default: '' },
   status: { type: Number, default: 0 },
-  isAdministrator: { type: Boolean }, // admin account
+  isAdministrator: { type: Boolean },      // admin account
 
-  alerts: { type: 'Mixed' },      // last read time of each kind of alerts
-                                  // e.g. { <alertId>: new Date() }
+  alerts: { type: 'Mixed' },               // last read time of each kind of alerts
+                                           // e.g. { <alertId>: new Date() }
   devices: {
     android: { type: Array, default: undefined }, // android
     ios: { type: Array, default: undefined },     // iphone
     web: { type: Array, default: undefined },     // web browser
-    last: { type: 'Mixed' },      // last device
-    invited: { type: 'Mixed' },   // device being invitated
+    last: { type: 'Mixed' },               // last device
+    invited: { type: 'Mixed' },            // device being invitated
   },
-  preferences: { type: 'Mixed' }, // preferences
-  invites: { type: 'Mixed' },     // invitation
+  preferences: { type: 'Mixed' },          // preferences
+  invites: { type: 'Mixed' },              // invitation
   socials: {
-    wechat: { type: 'Mixed' },
-    facebook: { type: 'Mixed' },
-    github: { type: 'Mixed' },
+    wechat: { type: 'Mixed' },             // wechat binding info
+    facebook: { type: 'Mixed' },           // facebook binding info
+    google: { type: 'Mixed' },             // google binding info
+    github: { type: 'Mixed' },             // github binding info
   },
   statistics: { type: 'Mixed' },
 };
