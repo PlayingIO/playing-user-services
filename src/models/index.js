30041617abc2becbd46040aacb6dd10d75fc95a4
forafish <fora.luo@gmail.com>
2018-03-06 03:18:21 +0800

    export permission/role schema

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 import glob from 'glob';
 import path from 'path';
+import { permission, role } from './permission-schema';
 
 // load all models
 let modelFiles = glob.sync(path.join(__dirname, './*-model.js'));
@@ -7,3 +8,6 @@ modelFiles.forEach(file => {
   let name = path.basename(file, '-model.js');
   module.exports[name] = require(file);
 });
+
+module.exports.permission = permission;
+module.exports.role = role;
