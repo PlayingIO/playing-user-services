d312106997f64a327732b5ffe11b19370d5f173b
forafish <fora.luo@gmail.com>
2018-03-09 16:12:30 +0800

    disallow other user-group methods

--- src/services/user-group/user-group-hooks.js
+++ src/services/user-group/user-group-hooks.js
@@ -1,4 +1,5 @@
 import { hooks as auth } from 'feathers-authentication';
+import { disallow } from 'feathers-hooks-common';
 import { hooks } from 'mostly-feathers-mongoose';
 
 module.exports = function(options = {}) {
@@ -6,7 +7,10 @@ module.exports = function(options = {}) {
     before: {
       all: [
         auth.authenticate('jwt')
-      ]
+      ],
+      create: disallow(),
+      update: disallow(),
+      remove: disallow()
     },
     after: {
       all: [
