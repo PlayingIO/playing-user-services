d4aa748c4673b56bdad7bfa7ea4cf3262e3ec48f
forafish <fora.luo@gmail.com>
2018-03-13 18:06:35 +0800

    add status of user-permission entity

--- src/entities/user-permission-entity.js
+++ src/entities/user-permission-entity.js
@@ -1,6 +1,17 @@
+import dateFn from 'date-fns';
 import Entity from 'mostly-entity';
 
 const UserPermissionEntity = new Entity('UserPermission');
+
+UserPermissionEntity.expose('status', {}, (ace) => {
+  const now = new Date();
+  if (ace.begin || ace.end) {
+    if (ace.begin && dateFn.isBefore(now, ace.begin)) return 'pending';
+    if (ace.begin && ace.end && dateFn.isWithinRange(now, ace.start, ace.end)) return 'effective';
+    if (ace.end && dateFn.isAfter(now, ace.end)) return 'archived';
+  }
+  return 'effective';
+});
 
 UserPermissionEntity.excepts('createdAt', 'updatedAt', 'destroyedAt');
 
