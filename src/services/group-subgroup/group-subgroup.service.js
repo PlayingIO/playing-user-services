8e2c1ba094c00e83dcc02eb76e8e6e9718ab1336
forafish <fora.luo@gmail.com>
2018-05-16 18:41:02 +0800

    add group/subgroups service

---
+++ src/services/group-subgroup/group-subgroup.service.js
@@ -0,0 +1,29 @@
+import assert from 'assert';
+import makeDebug from 'debug';
+import fp from 'mostly-func';
+
+import defaultHooks from './group-subgroup.hooks';
+
+const debug = makeDebug('playing:user-services:groups/subgroups');
+
+const defaultOptions = {
+  name: 'groups/subgroups'
+};
+
+export class GroupSubgroupService {
+  constructor (options) {
+    this.options = fp.assignAll(defaultOptions, options);
+    this.name = this.options.name;
+  }
+
+  setup (app) {
+    this.app = app;
+    this.hooks(defaultHooks(this.options));
+  }
+}
+
+export default function init (app, options, hooks) {
+  return new GroupSubgroupService(options);
+}
+
+init.Service = GroupSubgroupService;
